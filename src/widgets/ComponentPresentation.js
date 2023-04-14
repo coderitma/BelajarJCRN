4442873cc295f67a88ae019214b7dbfaa6e3ca3e
Yanzen Project <yanwarsolah@gmail.com>
2023-04-14 21:24:18 +0700

    STEP 02/01

--- src/widgets/ComponentPresentation.js
+++ src/widgets/ComponentPresentation.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet, Button, View } from "react-native";
+import { Text, StyleSheet, Button, View, TouchableOpacity } from "react-native";
 
 const ComponentPresentation = ({ onPress, counter, up, down, reset }) => {
   return (
@@ -10,10 +10,16 @@ const ComponentPresentation = ({ onPress, counter, up, down, reset }) => {
           Hello Component Presentation {counter}
         </Text>
       </View>
-      <View style={{ marginTop: 50 }}>
-        <Button onPress={() => reset()} title="Reset" />
-        <Button onPress={() => up()} title="Up" />
-        <Button onPress={() => down()} title="Down" />
+      <View style={{ marginTop: 50, width: "100%", paddingHorizontal: 16 }}>
+        <TouchableOpacity onPress={() => reset()} style={styles.button}>
+          <Text style={styles.buttonContent}>Reset</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => up()} style={styles.button}>
+          <Text style={styles.buttonContent}>Up</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => down()} style={styles.button}>
+          <Text style={styles.buttonContent}>Down</Text>
+        </TouchableOpacity>
       </View>
     </>
   );
@@ -21,10 +27,27 @@ const ComponentPresentation = ({ onPress, counter, up, down, reset }) => {
 
 const styles = StyleSheet.create({
   text: {
-    color: "#10a131",
+    color: "#333",
+    fontSize: 32,
   },
   textDanger: {
     color: "red",
+    fontSize: 32,
+  },
+  button: {
+    backgroundColor: "#580AFF",
+    padding: 10,
+    width: "100%",
+    borderRadius: 10,
+    marginVertical: 16,
+  },
+  buttonContent: {
+    color: "#fff",
+    textAlign: "center",
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
 
