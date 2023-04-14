0c68ead26a0decfda5ad5c4cf0005d245af9f613
Yanzen Project <yanwarsolah@gmail.com>
2023-04-14 21:01:51 +0700

    STEP 01/01

--- src/widgets/ComponentPresentation.js
+++ src/widgets/ComponentPresentation.js
@@ -1,11 +1,31 @@
-import { Text } from "react-native";
+import { Text, StyleSheet, Button, View } from "react-native";
 
-const ComponentPresentation = ({ onPress, counter }) => {
+const ComponentPresentation = ({ onPress, counter, up, down, reset }) => {
   return (
-    <Text onPress={() => onPress()}>
-      Hello Component Presentation {counter}
-    </Text>
+    <>
+      <View>
+        <Text
+          style={counter > 10 ? styles.textDanger : styles.text}
+          onPress={() => onPress()}>
+          Hello Component Presentation {counter}
+        </Text>
+      </View>
+      <View style={{ marginTop: 50 }}>
+        <Button onPress={() => reset()} title="Reset" />
+        <Button onPress={() => up()} title="Up" />
+        <Button onPress={() => down()} title="Down" />
+      </View>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    color: "#10a131",
+  },
+  textDanger: {
+    color: "red",
+  },
+});
 
 export default ComponentPresentation;
