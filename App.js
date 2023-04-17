138465173569b14d2311993a06ed9c8c4e1742ed
Yanzen Project <yanwarsolah@gmail.com>
2023-04-17 20:07:06 +0700

    step 02/27

--- App.js
+++ App.js
@@ -1,22 +1,103 @@
 import { useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+
+const DATA = [
+  {
+    kodeBarang: "BRG-112",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-111",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-101",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-1",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-006",
+    namaBarang: "Krisbow Battery A2",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-005",
+    namaBarang: "Yupi Candy Bear",
+    hargaBeli: 920,
+    hargaJual: 1000,
+    jumlahBarang: 120,
+  },
+  {
+    kodeBarang: "BRG-004",
+    namaBarang: "Happy Care Hand Sanitizer",
+    hargaBeli: 12000,
+    hargaJual: 15000,
+    jumlahBarang: 98,
+  },
+  {
+    kodeBarang: "BRG-002",
+    namaBarang: "Dompet Nevergate",
+    hargaBeli: 14000,
+    hargaJual: 23000,
+    jumlahBarang: 166,
+  },
+  {
+    kodeBarang: "BRG-908",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+];
+
+const ListItem = ({ item }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        backgroundColor: "#f45f45",
+        borderRadius: 8,
+      }}>
+      <View>
+        <Text>{item.namaBarang}</Text>
+        <Text>{item.kodeBarang}</Text>
+      </View>
+      <View>
+        <Text>{item.hargaJual}</Text>
+        <Text>{item.hargaBeli}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 export default function App() {
   return (
     <>
-      <View style={styles.heading}>
-        <Text style={styles.text}>Morning, Yanwar</Text>
-      </View>
       <View style={styles.container}>
-        <View style={styles.item1}>
-          <Text>Lorem ipsum</Text>
-        </View>
-        <View style={styles.item2}>
-          <Text>Dolor sir ameteru</Text>
-        </View>
-        <View style={styles.item3}>
-          <Text>Dolor sir ameteru</Text>
-        </View>
+        {DATA.map((item, index) => (
+          <ListItem item={item} key={index} />
+        ))}
       </View>
     </>
   );
@@ -33,12 +114,12 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    flexDirection: "row",
     justifyContent: "center",
-    // alignItems: "",
+    margin: 30,
     paddingVertical: 32,
-    gap: 20,
+    gap: 8,
   },
+
   item1: {
     padding: 10,
     height: "20%",
