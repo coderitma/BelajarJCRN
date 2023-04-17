01d2ed8c1df7ff7014a13ba36c9275cfe232d2bf
Yanzen Project <yanwarsolah@gmail.com>
2023-04-17 19:23:45 +0700

    step 01/27

--- App.js
+++ App.js
@@ -1,42 +1,61 @@
-import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import ComponentPresentation from "./src/widgets/ComponentPresentation";
-import { Button } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-
-  const up = () => {
-    setCounter(counter + 1);
-  };
-
-  const down = () => {
-    if (counter > 0) {
-      setCounter(counter - 1);
-    }
-  };
   return (
-    <View style={styles.container}>
-      <ComponentPresentation
-        down={down}
-        up={up}
-        reset={() => setCounter(0)}
-        counter={counter}
-        onPress={() => setCounter(counter + 1)}
-      />
-
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.heading}>
+        <Text style={styles.text}>Morning, Yanwar</Text>
+      </View>
+      <View style={styles.container}>
+        <View style={styles.item1}>
+          <Text>Lorem ipsum</Text>
+        </View>
+        <View style={styles.item2}>
+          <Text>Dolor sir ameteru</Text>
+        </View>
+        <View style={styles.item3}>
+          <Text>Dolor sir ameteru</Text>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  heading: {
+    marginTop: 50,
+    marginHorizontal: 10,
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    flexDirection: "row",
     justifyContent: "center",
+    // alignItems: "",
+    paddingVertical: 32,
+    gap: 20,
+  },
+  item1: {
+    padding: 10,
+    height: "20%",
+    backgroundColor: "#8e8e8e",
+    borderRadius: 10,
+  },
+  item2: {
+    padding: 10,
+    height: "20%",
+    backgroundColor: "#8e8e8e",
+    borderRadius: 10,
+  },
+  item3: {
+    padding: 10,
+    height: "20%",
+    backgroundColor: "#8e8e8e",
+    borderRadius: 10,
   },
 });
 
