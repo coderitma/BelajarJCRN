67c56f6cf94256dd3101fa209c8a0feac1632f5a
Yanzen Project <yanwarsolah@gmail.com>
2023-04-17 20:46:27 +0700

    step 04/27

--- App.js
+++ App.js
@@ -1,7 +1,266 @@
 import { useState } from "react";
-import { StyleSheet, View, Text, TouchableOpacity, Alert } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
+  FlatList,
+} from "react-native";
 
 const DATA = [
+  {
+    kodeBarang: "BRG-112",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-111",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-101",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-1",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-006",
+    namaBarang: "Krisbow Battery A2",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-005",
+    namaBarang: "Yupi Candy Bear",
+    hargaBeli: 920,
+    hargaJual: 1000,
+    jumlahBarang: 120,
+  },
+  {
+    kodeBarang: "BRG-004",
+    namaBarang: "Happy Care Hand Sanitizer",
+    hargaBeli: 12000,
+    hargaJual: 15000,
+    jumlahBarang: 98,
+  },
+  {
+    kodeBarang: "BRG-002",
+    namaBarang: "Dompet Nevergate",
+    hargaBeli: 14000,
+    hargaJual: 23000,
+    jumlahBarang: 166,
+  },
+  {
+    kodeBarang: "BRG-908",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-112",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-111",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-101",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-1",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-006",
+    namaBarang: "Krisbow Battery A2",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-005",
+    namaBarang: "Yupi Candy Bear",
+    hargaBeli: 920,
+    hargaJual: 1000,
+    jumlahBarang: 120,
+  },
+  {
+    kodeBarang: "BRG-004",
+    namaBarang: "Happy Care Hand Sanitizer",
+    hargaBeli: 12000,
+    hargaJual: 15000,
+    jumlahBarang: 98,
+  },
+  {
+    kodeBarang: "BRG-002",
+    namaBarang: "Dompet Nevergate",
+    hargaBeli: 14000,
+    hargaJual: 23000,
+    jumlahBarang: 166,
+  },
+  {
+    kodeBarang: "BRG-908",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-112",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-111",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-101",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-1",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-006",
+    namaBarang: "Krisbow Battery A2",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-005",
+    namaBarang: "Yupi Candy Bear",
+    hargaBeli: 920,
+    hargaJual: 1000,
+    jumlahBarang: 120,
+  },
+  {
+    kodeBarang: "BRG-004",
+    namaBarang: "Happy Care Hand Sanitizer",
+    hargaBeli: 12000,
+    hargaJual: 15000,
+    jumlahBarang: 98,
+  },
+  {
+    kodeBarang: "BRG-002",
+    namaBarang: "Dompet Nevergate",
+    hargaBeli: 14000,
+    hargaJual: 23000,
+    jumlahBarang: 166,
+  },
+  {
+    kodeBarang: "BRG-908",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-112",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-111",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-101",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-1",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-006",
+    namaBarang: "Krisbow Battery A2",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
+  {
+    kodeBarang: "BRG-005",
+    namaBarang: "Yupi Candy Bear",
+    hargaBeli: 920,
+    hargaJual: 1000,
+    jumlahBarang: 120,
+  },
+  {
+    kodeBarang: "BRG-004",
+    namaBarang: "Happy Care Hand Sanitizer",
+    hargaBeli: 12000,
+    hargaJual: 15000,
+    jumlahBarang: 98,
+  },
+  {
+    kodeBarang: "BRG-002",
+    namaBarang: "Dompet Nevergate",
+    hargaBeli: 14000,
+    hargaJual: 23000,
+    jumlahBarang: 166,
+  },
+  {
+    kodeBarang: "BRG-908",
+    namaBarang: "Kotak Pandora",
+    hargaBeli: 7000,
+    hargaJual: 7400,
+    jumlahBarang: 200,
+  },
   {
     kodeBarang: "BRG-112",
     namaBarang: "Kotak Pandora",
@@ -78,8 +337,8 @@ const ListItem = ({ item }) => {
         paddingHorizontal: 20,
         paddingVertical: 10,
         backgroundColor: "#e3e3e3",
-        borderRadius: 8,
-        elevation: 2,
+        borderBottomWidth: 0.5,
+        borderBottomColor: "gray",
       }}>
       <View>
         <Text>{item.namaBarang}</Text>
@@ -94,12 +353,31 @@ const ListItem = ({ item }) => {
 };
 
 export default function App() {
+  const [isRefresh, setRefresh] = useState(false);
+
+  const callAPI = () => {
+    setRefresh(true);
+    console.log("Refresh kepanggil");
+    setTimeout(() => setRefresh(false), 3000);
+  };
+
+  const infiniteScroll = () => {
+    setRefresh(true);
+    console.log("Refresh kebawah kepanggil");
+    setTimeout(() => setRefresh(false), 3000);
+  };
+
   return (
     <>
       <View style={styles.container}>
-        {DATA.map((item, index) => (
-          <ListItem item={item} key={index} />
-        ))}
+        <FlatList
+          onRefresh={callAPI}
+          onEndReached={infiniteScroll}
+          refreshing={isRefresh}
+          data={DATA}
+          renderItem={({ item }) => <ListItem item={item} />}
+          keyExtractor={(item, index) => index}
+        />
       </View>
     </>
   );
@@ -117,7 +395,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    margin: 30,
+    marginVertical: 20,
     paddingVertical: 32,
     gap: 8,
   },
