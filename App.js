0f821361db8b136edcc47db28f41804dabc84b01
Yanzen Project <yanwarsolah@gmail.com>
2023-04-18 19:04:32 +0700

    first step

--- App.js
+++ App.js
@@ -1,34 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import ComponentPresentation from "./src/widgets/ComponentPresentation";
-import { Button } from "react-native";
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-
-  const up = () => {
-    setCounter(counter + 1);
-  };
-
-  const down = () => {
-    if (counter > 0) {
-      setCounter(counter - 1);
-    }
-  };
-  return (
-    <View style={styles.container}>
-      <ComponentPresentation
-        down={down}
-        up={up}
-        reset={() => setCounter(0)}
-        counter={counter}
-        onPress={() => setCounter(counter + 1)}
-      />
-
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
