ba225e2b9662146b1e8725cfa96c58a3092309c6
Yanzen Project <yanwarsolah@gmail.com>
2023-05-02 20:23:29 +0700

    step 02/30

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import FormSignin from "./src/widgets/FormSignin";
 import { ScrollView } from "react-native";
+import ScreenProductDetail from "./src/screens/products/ScreenProductDetail";
 
 export default function App() {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <FormSignin />
-    </ScrollView>
+    // <ScrollView contentContainerStyle={styles.container}>
+    <ScreenProductDetail />
+    // </ScrollView>
   );
 }
 
--- src/screens/products/ScreenProductDetail.js
+++ src/screens/products/ScreenProductDetail.js
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, Text } from "react-native";
 
 const ScreenProductDetail = () => {
-  const [product, setProduct] = useState({});
+  const [product, setProduct] = useState();
+  const [complete, setComplete] = useState(false);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    setComplete(false);
+    axios
+      .get("https://fakestoreapi.com/products/1")
+      .then((response) => {
+        setProduct(response.data);
+      })
+      .catch((error) => {})
+      .finally(() => {
+        setComplete(true);
+      });
+  }, []);
 };
 
 export default ScreenProductDetail;
