820238d60aafeb7e929e0a0a64a403581a90bb45
Yanzen Project <yanwarsolah@gmail.com>
2023-05-02 19:42:12 +0700

    step 02/30

---
+++ src/screens/products/ScreenProductDetail.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+const ScreenProductDetail = () => {
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {}, []);
+};
+
+export default ScreenProductDetail;
