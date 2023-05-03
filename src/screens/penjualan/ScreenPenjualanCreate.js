471540899f12c3cd4e7e98630fc27a9030a7b815
Yanzen Project <yanwarsolah@gmail.com>
2023-05-03 19:47:50 +0700

    step 04/31

--- src/screens/penjualan/ScreenPenjualanCreate.js
+++ src/screens/penjualan/ScreenPenjualanCreate.js
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import { Text } from "react-native";
-import { Appbar, List, MD3Colors, TextInput } from "react-native-paper";
+import {
+  Appbar,
+  Divider,
+  List,
+  MD3Colors,
+  RadioButton,
+  TextInput,
+} from "react-native-paper";
 
 const ScreenPenjualanCreate = () => {
   const [items, setItems] = useState([]);
@@ -17,9 +24,10 @@ const ScreenPenjualanCreate = () => {
         <Appbar.BackAction onPress={() => {}} />
         <Appbar.Content title="Point Of Sales" />
       </Appbar.Header>
-
+      <Divider />
       <List.Section>
         <List.Subheader>Items</List.Subheader>
+        <Divider />
         <List.Item
           title="Lorem Ipsum 1"
           description="Rp. 12000 x 2"
@@ -36,13 +44,28 @@ const ScreenPenjualanCreate = () => {
           right={() => <Text>Rp. 24.000</Text>}
         />
       </List.Section>
-
+      <Divider />
       <List.Section>
         <List.Subheader>Pembayaran</List.Subheader>
+        <Divider />
         <List.Item title="Total" right={() => <Text>Rp. 72.000</Text>} />
         <List.Item title="Change" right={() => <Text>Rp. 0</Text>} />
       </List.Section>
-      <TextInput style={{ marginHorizontal: 16 }} mode="outlined" label="Pay" />
+      <RadioButton.Group
+        onValueChange={(value) => {
+          setPayment((values) => ({ ...values, method: value }));
+        }}
+        value={payment.method}>
+        <RadioButton.Item label="Cash" value="cash" />
+        <RadioButton.Item label="Bank Transfer" value="bank" />
+      </RadioButton.Group>
+      <Divider />
+      <TextInput
+        value={`${payment.pay || ""}`}
+        style={{ marginHorizontal: 16, marginTop: 16 }}
+        mode="outlined"
+        label="Pay"
+      />
     </>
   );
 };
